11a239821b3deabd47ee6bb1c47722b0bbbafc47
vsloboda <vsloboda@ittouch.pl>
2018-07-04 19:00:53 +0200

    chat component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { ChatComponent } from './chat/chat.component';
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot( routes, { useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
